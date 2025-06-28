9bd375300d5ea7eb87a0c3123875ea512c95570f
Wrrohit788 <154300370+Wrrohit788@users.noreply.github.com>
2025-06-28 13:16:20 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -7,7 +7,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("UltroidOfficial")
+    return web.json_response("wrrohit02")
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
